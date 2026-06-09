e88a084fb9f9efd64c4949cfc8f43c2b3b591d85
25LP5308 25LP5308 <telmuunotgonbileg@gmail.com>
2026-06-09 12:12:01 +0800

    Redirect root to admin

--- apps/chatbot/app/page.tsx
+++ apps/chatbot/app/page.tsx
@@ -1,10 +1,5 @@
-import { Suspense } from "react";
-import HomeClient from "./HomeClient";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  return (
-    <Suspense fallback={null}>
-      <HomeClient />
-    </Suspense>
-  );
+  redirect("/admin");
 }
